c2a9558afd929dabd415a28ddf10734427b15266
julion-m <julianmoeller97@gmail.com>
2018-11-25 00:37:23 +0100

    new README and one new Kata

---
+++ src/main/DontGiveMeFive.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.util.stream.IntStream;
+
+public class DontGiveMeFive {
+
+    public static int dontGiveMeFive(int start, int end){
+        int result = 0;
+
+        IntStream.range(start, end).filter(n -> (n % 5 != 0) && (n % 10 ))
+    }
+}
---
+++ src/main/Max.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Max {
+
+    public static int sequence(int[] arr) {
+
+
+        return 0;
+    }
+}
